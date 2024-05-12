c73b523ea29671161906ad50314a48371daffa2f
Cocotterooo <fcocoterooo@gmail.com>
2024-05-12 23:18:06 +0200

    fix lang redes sociais

--- linktree_dai/linktree_dai/components/links/links_column.py
+++ linktree_dai/linktree_dai/components/links/links_column.py
@@ -7,6 +7,9 @@ from linktree_dai.data.LinkData import LinkData
 # Styles:
 from linktree_dai.styles.styles import Size
 
+# State:
+from linktree_dai.pages.index.index_state import IndexState
+
 def links_column(links: list[LinkData]) -> rx.Component:
     return rx.vstack(
         # GENERAL SECTION:
@@ -18,7 +21,12 @@ def links_column(links: list[LinkData]) -> rx.Component:
         rx.cond(
             len([data for data in links if data.is_social_media and data.is_active]) > 0,
             rx.vstack(
-                rx.heading('Redes Sociales'),
+                rx.heading(rx.cond(
+                    IndexState.lang_mode,
+                    'Redes Sociales',
+                    'Redes Sociais'
+                    )
+                ),
                 *[
                     link_buttom(data)
                     for data in links if data.is_social_media and data.is_active
